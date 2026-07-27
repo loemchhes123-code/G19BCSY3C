1bf91391dbb3927383cc6ed10e4ee894619e6060
loemchhes <loemchhes123@gmail.com>
2026-07-27 11:02:45 +0700

    sesion3.5 and 3.6 with 3.7

--- docker/laravel/entrypoint.development.sh
+++ docker/laravel/entrypoint.development.sh
@@ -3,8 +3,10 @@ set -e
 
 composer install
 wait $!
+npm install
+wait $!
 php artisan key:generate
 wait $!
-php artisan migrate
-wait $!
-php artisan serve --host=0.0.0.0 --port=8000
+# php artisan migrate --force
+# wait $!
+exec supervisord -c /etc/supervisor/conf.d/supervisord.development.conf
